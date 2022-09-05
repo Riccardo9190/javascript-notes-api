2b9090c41e81f9a4cbf7a2f33623ca6af16cf36f
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2022-09-05 11:51:32 -0300

    Deploy

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
 require('./config/database');
 
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
+var express = require('express');
+var logger = require('morgan');
+var cors = require('cors');
 
-const usersRouter = require('./src/routes/users');
-const notesRouter = require('./src/routes/notes');
+var usersRouter = require('./src/routes/users');
+var notesRouter = require('./src/routes/notes');
 
-const app = express();
-
-const corsOptions = {
-  "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": true,
-  "optionsSuccessStatus": 204
-}
+var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use(cors(corsOptions))
+app.use(cors());
 
 app.use('/users', usersRouter);
-app.use('/notes', notesRouter)
+app.use('/notes', notesRouter);
 
 module.exports = app;
