2b0b90d1493e71c7d5e79ed886440b9829cdee32
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2022-09-05 12:04:16 -0300

    Deploy

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,14 +3,6 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken')
 const express = require('express');
 const WithAuth = require('../middlewares/auth');
-const cors = require('cors')
-
-const corsOptions = {
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-  optionsSuccessStatus: 204
-}
 
 const router = express.Router();
 const secret = process.env.JWT_TOKEN;
@@ -28,7 +20,7 @@ router.post('/register', async (req, res) => {
   }
 });
 
-router.post('/login', cors(corsOptions),async (req, res) => {
+router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
