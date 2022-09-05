5781d6ab81429965b54a5670905ac1aa58de1da6
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2022-09-05 12:57:34 -0300

    Deploy

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ const notesRouter = require('./src/routes/notes');
 
 const app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
--- src/routes/users.js
+++ src/routes/users.js
@@ -8,12 +8,6 @@ const app = express();
 const router = express.Router();
 const secret = process.env.JWT_TOKEN;
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 router.post('/register', async (req, res) => {
   const { name, email, password } = req.body;
   const user = new User({ name, email, password });
