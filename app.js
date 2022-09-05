a0da1d82e94764fc15c7baf22e62a7b28be28733
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2022-09-05 18:22:26 -0300

    Deploy

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ require('./config/database');
 
 const express = require('express');
 const logger = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 // const corsHeaders = require('./middleware/cors-headers');
 
-const corsOptions = {
-  origin: 'https://js-notes-clnt.herokuapp.com',
-  optionsSuccessStatus: 200
-}
+// const corsOptions = {
+//   origin: 'https://js-notes-clnt.herokuapp.com',
+//   optionsSuccessStatus: 200
+// }
 
 const usersRouter = require('./src/routes/users');
 const notesRouter = require('./src/routes/notes');
@@ -18,8 +18,21 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use(cors(corsOptions))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  )
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "POST, PUT, PATCH, GET, DELETE"
+    )
+    return res.status(200).json({})
+  }
+  next()
+})
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
