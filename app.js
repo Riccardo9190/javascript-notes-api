0d21cf4db76b57b0682bbf0be3b42f12f4936136
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2022-09-05 11:23:13 -0300

    Deploy

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers: Content-Type")
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Headers", 'Content-Type')
   res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
   app.use(cors());
   next();
