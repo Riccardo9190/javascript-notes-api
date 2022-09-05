8d107048822a7df4b1e9bc5dd9aa9cff8d483915
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2022-09-05 12:08:06 -0300

    Deploy

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ const usersRouter = require('./src/routes/users');
 const notesRouter = require('./src/routes/notes');
 
 const app = express();
+const corsOptions = {
+    "origin": 'https://js-notes-clnt.herokuapp.com/login',
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "optionsSuccessStatus": 200
+}
+
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.options('*', cors()) 
+app.use(cors(corsOptions))
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
