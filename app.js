cf7123b5058484a5f672ffd4e47e552a7a4671c4
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2022-09-05 15:17:18 -0300

    Deploy

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
 
+
 const usersRouter = require('./src/routes/users');
 const notesRouter = require('./src/routes/notes');
 
@@ -12,26 +13,20 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-
-  res.header("Access-Control-Allow-Origin", "*");
-
-  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE,OPTIONS');
-
-  res.header("Access-Control-Allow-Headers", "Content-type,Accept,X-Custom-Header");
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-
-  app.use(cors());
-
-  next();
-});
+var whitelist = [
+  '*',
+];
+var corsOptions = {
+  origin: function(origin, callback){
+      var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+      callback(null, originIsWhitelisted);
+  },
+  credentials: true
+};
+app.use(cors(corsOptions));
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
 
 module.exports = app;
+
