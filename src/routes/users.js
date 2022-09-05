0f08761a0ce04273377f49df4986aace520f2d29
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2022-09-05 12:45:52 -0300

    Deploy

--- src/routes/users.js
+++ src/routes/users.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const secret = process.env.JWT_TOKEN;
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://js-notes-clnt.herokuapp.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
