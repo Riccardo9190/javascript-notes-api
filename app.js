743a7eb3a6f47e512acc338928084664a116044f
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2022-09-05 12:15:56 -0300

    Deploy

--- app.js
+++ app.js
@@ -8,18 +8,17 @@ const usersRouter = require('./src/routes/users');
 const notesRouter = require('./src/routes/notes');
 
 const app = express();
-const corsOptions = {
-    "origin": 'https://js-notes-clnt.herokuapp.com/login',
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": true,
-    "optionsSuccessStatus": 200
-}
 
+app.use((req, res, next) => {
+  res.header("Acess-Control-Allow-Origin", "*");
+  app.use(cors());
+  next();
+})
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors(corsOptions))
+
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
